80af31ab3239f6020b59bdf485bf1e16ee060a49
Will Eccles <william.r.eccles@bloomy.com>
2024-05-22 15:53:52 -0400

    license: apply header to sources

--- src/absscpi/__init__.py
+++ src/absscpi/__init__.py
@@ -1,1 +1,6 @@
+# Copyright (c) 2024, Bloomy Controls, Inc. All rights reserved.
+#
+# Use of this source code is governed by a BSD-3-clause license that can be
+# found in the LICENSE file or at https://opensource.org/license/BSD-3-Clause
+
 from .driver import *
--- src/absscpi/driver.py
+++ src/absscpi/driver.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024, Bloomy Controls, Inc. All rights reserved.
+#
+# Use of this source code is governed by a BSD-3-clause license that can be
+# found in the LICENSE file or at https://opensource.org/license/BSD-3-Clause
+
 from ctypes import *
 from ctypes.util import find_library
 import platform
